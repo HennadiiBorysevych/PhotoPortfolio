83ac744726d59594cf39d04616b045687bef2734
Hennadii Borysevych <genuch3@gmail.com>
2023-06-22 19:20:04 +0300

    feat:routes map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+
+import Header from "./components/Header";
+import AnimRoutes from "./components/AnimRoutes";
+
+import { BrowserRouter } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <AnimRoutes />
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
